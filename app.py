be3f5f814e2f0f428d93858b883d68273e13fca8
TheBrainGamingSingh <itsmanpreetsinghjuneja@gmail.com>
2021-05-23 16:53:45 +0530

    try to fix: cowin api

--- app.py
+++ app.py
@@ -38,15 +38,23 @@ parser.add_argument('cluster_data')
 MODEL_PATH = './model/RandomForest.pkl'
 BEARER_TOKEN = 'pec_capstone_group_12'
 BASE_URL = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id={}&date={}"
-HEADERS = {
-'authorization': 'Bearer {}'.format(BEARER_TOKEN),
+HEADERS = { 'Authorization': 'Bearer {}'.format(BEARER_TOKEN),
 'authority': 'cdn-api.co-vin.in',
-'scheme' : 'https',
+'scheme': 'https',
 'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
 'accept-encoding': 'gzip, deflate, br',
 'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
+'cache-control': 'max-age=0',
+'if-none-match': 'W/"5e92-TFnEiLM122Z1S6FKhuTvWWz+GQA"',
+'sec-fetch-dest': 'document',
+'sec-fetch-mode': 'navigate',
+'sec-fetch-site': 'none',
+'sec-fetch-user' : '?1',
+'sec-gpc' : '1',
+'upgrade-insecure-requests': '1',
 'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36'
 }
+
 COLUMNS = ['vaccine','center_id', 'name','address','min_age_limit','pincode','available_capacity_dose1','available_capacity_dose2']
 
 
