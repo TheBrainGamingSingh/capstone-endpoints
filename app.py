04389e0f997d73c91e78e040faf95edcd4c516ef
TheBrainGamingSingh <itsmanpreetsinghjuneja@gmail.com>
2021-05-23 22:34:19 +0530

    added UI

--- app.py
+++ app.py
@@ -40,19 +40,6 @@ BEARER_TOKEN = 'pec_capstone_group_12'
 BASE_URL = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id={}&date={}"
 # 'Authorization': 'Bearer {}'.format(BEARER_TOKEN),
 HEADERS = {
-'authority': 'cdn-api.co-vin.in',
-'scheme': 'https',
-'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-'accept-encoding': 'gzip, deflate, br',
-'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
-'cache-control': 'max-age=0',
-'if-none-match': 'W/"5e92-TFnEiLM122Z1S6FKhuTvWWz+GQA"',
-'sec-fetch-dest': 'document',
-'sec-fetch-mode': 'navigate',
-'sec-fetch-site': 'none',
-'sec-fetch-user' : '?1',
-'sec-gpc' : '1',
-'upgrade-insecure-requests': '1',
 'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36'
 }
 
@@ -79,7 +66,7 @@ def get_vaccine_details(district_id, query_date):
         return response_df.to_json(orient="split")
 
     return {'error' : 'No details found',
-            'response': str(response)}
+            'response': str(response.text)}
 # Endpoints
 class ComplaintClassifier(Resource):
     def get(self):
